a17c100f7c415cf37d29c36cdb8342832ddcf29a
Gabriel Gelius-Dietrich <geliudie@uni-duesseldorf.de>
2013-01-16 00:00:00 +0000

    version 1.1.16

--- R/uglyHack.R
+++ R/uglyHack.R
@@ -1,25 +1,25 @@
 #  uglyHack.R
 #  FBA and friends with R.
 #
-#  Copyright (C) 2010-2012 Gabriel Gelius-Dietrich, Dpt. for Bioinformatics,
+#  Copyright (C) 2010-2013 Gabriel Gelius-Dietrich, Dpt. for Bioinformatics,
 #  Institute for Informatics, Heinrich-Heine-University, Duesseldorf, Germany.
 #  All right reserved.
 #  Email: geliudie@uni-duesseldorf.de
 #
 #  This file is part of sybilSBML.
 #
-#  SyBiL is free software: you can redistribute it and/or modify
+#  SybilSBML is free software: you can redistribute it and/or modify
 #  it under the terms of the GNU General Public License as published by
 #  the Free Software Foundation, either version 3 of the License, or
 #  (at your option) any later version.
 #
-#  SyBiL is distributed in the hope that it will be useful,
+#  SybilSBML is distributed in the hope that it will be useful,
 #  but WITHOUT ANY WARRANTY; without even the implied warranty of
 #  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
 #  GNU General Public License for more details.
 #
 #  You should have received a copy of the GNU General Public License
-#  along with SyBiL.  If not, see <http://www.gnu.org/licenses/>.
+#  along with SybilSBML.  If not, see <http://www.gnu.org/licenses/>.
 
 
 ################################################
